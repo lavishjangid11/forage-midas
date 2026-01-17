6dac9855ff3c1433d2f32cdc8db2ce25bc36f195
Lavish Jangid <lavishjangid230719@acropolis.in>
2026-01-17 22:15:02 +0530

    Add RestTemplate bean to MidasCoreApplication
    Added a RestTemplate bean for making HTTP requests.

--- src/main/java/com/jpmc/midascore/MidasCoreApplication.java
+++ src/main/java/com/jpmc/midascore/MidasCoreApplication.java
@@ -3,6 +3,12 @@ package com.jpmc.midascore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Bean
+public RestTemplate restTemplate() {
+    return new RestTemplate();
+}
+
+
 @SpringBootApplication
 public class MidasCoreApplication {
 
