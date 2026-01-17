62e6c4b583af0f32d3c85f91dccf3b9e75fec7b2
Lavish Jangid <lavishjangid230719@acropolis.in>
2026-01-17 22:00:54 +0530

    Create TransactionListener.java

---
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -0,0 +1,53 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.Transaction;
+import com.jpmc.midascore.entity.TransactionRecord;
+import com.jpmc.midascore.entity.User;
+import com.jpmc.midascore.repository.TransactionRecordRepository;
+import com.jpmc.midascore.repository.UserRepository;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class TransactionListener {
+
+    private final UserRepository userRepository;
+    private final TransactionRecordRepository recordRepository;
+
+    public TransactionListener(UserRepository userRepository,
+                               TransactionRecordRepository recordRepository) {
+        this.userRepository = userRepository;
+        this.recordRepository = recordRepository;
+    }
+
+    @KafkaListener(topics = "${kafka.topic}")
+    @Transactional
+    public void listen(Transaction tx) {
+
+        var senderOpt = userRepository.findById(tx.getSenderId());
+        var recOpt = userRepository.findById(tx.getRecipientId());
+
+        // 1. Validate users
+        if (senderOpt.isEmpty() || recOpt.isEmpty())
+            return;
+
+        User sender = senderOpt.get();
+        User recipient = recOpt.get();
+
+        // 2. Validate balance
+        if (sender.getBalance().compareTo(tx.getAmount()) < 0)
+            return;
+
+        // 3. Update balances
+        sender.setBalance(sender.getBalance().subtract(tx.getAmount()));
+        recipient.setBalance(recipient.getBalance().add(tx.getAmount()));
+
+        // 4. Persist transaction
+        TransactionRecord record =
+            new TransactionRecord(tx.getAmount(), sender, recipient);
+
+        recordRepository.save(record);
+    }
+}
