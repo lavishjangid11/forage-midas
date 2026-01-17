910661a87fdf179324c408045c8747d652a00ab2
Lavish Jangid <lavishjangid230719@acropolis.in>
2026-01-17 21:54:18 +0530

    Create TransactionRecord.java

---
+++ TransactionRecord.java
@@ -0,0 +1,33 @@
+package com.jpmc.midascore.entity;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class TransactionRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    private User sender;
+
+    @ManyToOne
+    private User recipient;
+
+    public TransactionRecord() {}
+
+    public TransactionRecord(BigDecimal amount, User sender, User recipient) {
+        this.amount = amount;
+        this.sender = sender;
+        this.recipient = recipient;
+    }
+
+    // getters and setters
+    public BigDecimal getAmount() { return amount; }
+    public User getSender() { return sender; }
+    public User getRecipient() { return recipient; }
+}
